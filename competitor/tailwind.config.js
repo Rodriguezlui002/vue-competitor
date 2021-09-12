4f5d6288622308376197b73c86dfd0facccaecb4
Rodriguezlui002 <58277521+Rodriguezlui002@users.noreply.github.com>
2021-09-12 12:01:15 -0700

    Login in page improved.

--- competitor/tailwind.config.js
+++ competitor/tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
       colors: {
         'primary': '#ff570a',
         'secondary': '#17BEBB',
-        'secondary-active': '#0d6d6b'
+        'secondary-active': '#0d6d6b',
+        'tertiary': '#6c63ff',
+        'tertiary-active': '#3d33ff'
       }
     },
   },
